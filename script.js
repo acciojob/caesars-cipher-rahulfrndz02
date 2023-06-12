42f52e8784a9e2e6c03f2d94d814c00b4188b6f7
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-12 17:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0; i<str.length; i++){
-		if(lookup(str[i]) == undefined){
+		if(lookup[str[i]] == undefined){
 			decodedArr.push(str[i]);
 		}
 		else
