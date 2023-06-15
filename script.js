6d03034ee9c9cc9657c1de58bdc394f33b55ec77
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-15 15:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,10 +29,11 @@ const lookup = {
   Z: "M",
   "?": "?",
   ",": ",",
+  " ": " "
 };
 
 function rot13(encodedStr) {
- let decodedArr = ""; // Your Result goes here
+  let decodedArr = ""; // Your Result goes here
   // Only change code below this line
 	for(let i=0;i<encodedStr.length;i++){
 		let a=encodedStr.charAt(i);
